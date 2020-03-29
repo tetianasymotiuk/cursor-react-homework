837e4b38ea4cc642df7927c232dc64b7e2cdd263
Tetiana <tetianaprivat@gmail.com>
2020-03-29 19:53:47 +0300

    react hw01 - done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+import Post from './components/Post/Post';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const ANAKIN_IMAGE = "https://upload.wikimedia.org/wikipedia/en/thumb/7/74/Anakin-Jedi.jpg/220px-Anakin-Jedi.jpg";
+
+  const RAY_IMAGE = "https://specials-images.forbesimg.com/imageserve/5e63b3c8e1e617000759130e/960x0.jpg?fit=scale";
+
+  const propsData = {
+    authorData: {
+      name: "Anakin skywalker",
+      photo: ANAKIN_IMAGE,
+      nickname: "@dart_vader",
+    },
+    content: "WTF? Who is Ray? Why she is Skywalker? Luke...?",
+    image: RAY_IMAGE,
+    date: "26 февр.",
+  };
+  return <div className="test-data"><Post {...propsData} /></div>;
 }
 
 export default App;
